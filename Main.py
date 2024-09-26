65a85e8f74c9e089b9e87a3b10637ceee4993078
debrunei000 <debrunei000@warren.k12.in.us>
2024-09-26 09:16:04 -0400

    Done?

--- Main.py
+++ Main.py
@@ -27,68 +27,32 @@ BLACK = (0 ,0 ,0)
 font = pygame.font.Font('AttackGraffiti.ttf', 32)
 
 #Set Text for Score
-'''
-variable names:  score_text, score_rect
-render text: "Score: " + str(score)
-antialias: True
-color: GREEN
-background: DARKGREEN
-rect location: topleft = (10, 10)  
-'''
+
 score_text = font.render("Score: " + str(score), True, GREEN, DARKGREEN)
 score_rect = score_text.get_rect()
 score_rect.topleft = (10, 10)
 
 #Set Text for Title (Similar to Score)
-'''
-variable names:  title_text , title_rect 
-render text: "Feed the Dragon"
-antialias: True
-color: GREEN
-background: WHITE
-rect location: centerx = WINDOW_WIDTH//2
-rect location: y = 10 
-'''
+
 title_text = font.render("Feed the dragon", True, GREEN, WHITE)
 title_rect = title_text.get_rect()
 title_rect.centerx = Window_Width//2
 title_rect.y = 10
 
 #Set Text for Lives (Similar to Score)
-'''
-variable names:  lives_text, lives_rect
-render text: "Lives: " + str(player_lives)
-antialias: True
-color: GREEN
-background: DARKGREEN
-rect location: y = 10  
-'''
+
 lives_text = font.render("Lives: " + str(player_lives), True, GREEN, DARKGREEN)
 lives_rect = lives_text.get_rect()
 lives_rect.topright = (Window_Width - 10, 10)
 
 #Set Text for Game Over (Similar to Score)
-'''
-variable names:  game_over_text , game_over_rect 
-render text: "GAMEOVER"
-antialias: True
-color: GREEN
-background: DARKGREEN
-rect location: center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2) 
-'''
+
 game_over_text = font.render("GAMEOVER", True, GREEN, DARKGREEN)
 game_over_rect = game_over_text.get_rect()
 game_over_rect.center = (Window_Width//2, Window_Height//2)
 
 #Set Text for Continue (Similar to Score)
-'''
-variable names:  continue_text, continue_rect  
-render text: "Press any key to play again"
-antialias: True
-color: GREEN
-background: DARKGREEN
-rect location: center = (WINDOW_WIDTH//2, WINDOW_HEIGHT//2 + 32)
-'''
+
 continue_text = font.render("Press any key to continue", True, GREEN, DARKGREEN)
 continue_rect = continue_text.get_rect()
 continue_rect.center = (Window_Width//2, Window_Height//2 + 32)
